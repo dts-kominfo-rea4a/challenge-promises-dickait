d5d130370c1bd81b0131a578b9eadb5090fdadb1
Dicka Ariptian R <dickaariptian@gmail.com>
2022-09-16 22:13:43 +0700

    optimize code

--- promise.js
+++ promise.js
@@ -4,12 +4,10 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 const promiseOutput = async (data) => {
   const ixx = await promiseTheaterIXX();
   const vgc = await promiseTheaterVGC();
-  let output = 0;
-  for (const key in ixx) {
-    ixx[key].hasil === data ? output++ : output;
-  }
-  for (const key in vgc) {
-    vgc[key].hasil === data ? output++ : output;
+  let array = ixx.concat(vgc);
+  let output = 0
+  for (const key in array) {
+    array[key].hasil === data ? output++ : output;
   }
   return output;
 };
