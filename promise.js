93b4ed346a3583f484730b00aab6d5966326dc7b
Dicka Ariptian R <dickaariptian@gmail.com>
2022-09-16 21:54:36 +0700

    done promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (data) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+  let output = 0;
+  for (const key in ixx) {
+    ixx[key].hasil === data ? output++ : output;
+  }
+  for (const key in vgc) {
+    vgc[key].hasil === data ? output++ : output;
+  }
+  return output;
+};
 
 module.exports = {
   promiseOutput,
